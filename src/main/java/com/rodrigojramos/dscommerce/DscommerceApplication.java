ad42bc226f8e38a238bf3bd9285183f2d688bba2
Rodrigo Ramos <digojramos@gmail.com>
2023-01-18 16:41:07 -0300

    coded password

--- src/main/java/com/rodrigojramos/dscommerce/DscommerceApplication.java
+++ src/main/java/com/rodrigojramos/dscommerce/DscommerceApplication.java
@@ -1,7 +1,10 @@
 package com.rodrigojramos.dscommerce;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class DscommerceApplication {
@@ -10,5 +13,4 @@ public class DscommerceApplication {
 
 		SpringApplication.run(DscommerceApplication.class, args);
 	}
-
 }
